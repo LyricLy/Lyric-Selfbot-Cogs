1f4279b9e63c5613b52ce3bfd17588d365c8a164
LyricLy <hanson.twentytwo@gmail.com>
2017-06-30 12:56:16 +1200

    fix stuff

--- lyric_commands.py
+++ lyric_commands.py
@@ -6,12 +6,8 @@ import re
 import json
 import string
 import urllib
-from appuselfbot import bot_prefix
 from discord.ext import commands
 '''Module for custom commands.'''
-
-codes = requests.get("http://lyricly.tk/langs.json").text
-lang_codes = json.loads(codes)
 
 class LyricCommands:
 
@@ -36,13 +32,13 @@ class LyricCommands:
             text = msg
             window_size = int(len(text)*0.5)
             if window_size > 14: window_size = 14
-        await self.bot.edit_message(ctx.message, ":robot: " + text[0:window_size+1])
+        await self.bot.edit_message(ctx.message, text[0:window_size+1])
         letter = 0
         # all commented out code here is for debugging
         # times = []
         # start = time.time()
-        delay = (len(text)-window_size) * 0.03 #depending on the string size we can get away with more speed with less freezes
-        if delay > 0.5: delay = 0.5 #although eventually it's just wasteful
+        delay = (len(text)-window_size) * 0.03 # depending on the string size we can get away with more speed with less freezes
+        if delay > 0.5: delay = 0.5  # although eventually it's just wasteful
         for _ in text:
             # iteration_start = time.time()
             try:
@@ -51,14 +47,14 @@ class LyricCommands:
                 break
             letter += 1
             await asyncio.sleep(delay)
-            await self.bot.edit_message(ctx.message, ":robot: " + text[letter:letter+window_size+1])
+            await self.bot.edit_message(ctx.message, text[letter:letter+window_size+1])
             # iteration_end = time.time()
             # times.append(iteration_end - iteration_start)
         # end = time.time()
         # seconds = end - start
         # speed = len(text) / seconds
         # stability = max(times)
-        await self.bot.edit_message(ctx.message, ":robot: {}".format(text))
+        await self.bot.edit_message(ctx.message, "{} {}".format(self.bot.bot_prefix, text))
         
     @commands.command(pass_context=True)
     async def kekify(self, ctx, *, text):
@@ -66,6 +62,8 @@ class LyricCommands:
         for char in text:
             if char == "k":
                 result += "kek"
+            elif char == "K":
+                result += "KEK"
             else:
                 result += char
         await self.bot.edit_message(ctx.message, result)
